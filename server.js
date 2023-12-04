9a23be077e6ce018fc60b51121d4324e2a4c4869
nshkulkarni <105520469+nshkulkarni@users.noreply.github.com>
2023-12-04 14:57:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -79,9 +79,14 @@ app.get("/api/song", async (req, res) => {
     try{
         const { title } = req.query;
 
-        //
+        const query = `
+            SELECT s.*, a.a_name 
+            FROM song s 
+            JOIN artist a ON s.a_serial = a.a_serial 
+            WHERE s.s_title ILIKE $1
+        `;
 
-        const song = await pool.query("SELECT * FROM song WHERE s_title ILIKE $1", 
+        const song = await pool.query(query, 
         [`%${ title }%`]
         );
         res.json(song.rows);
@@ -90,8 +95,60 @@ app.get("/api/song", async (req, res) => {
     }
 });
 
-// Example endpoint for song search
-// query parameters- being able to send data within URL
+// Endpoint for creating a new playlist
+app.post("/api/playlists", async (req, res) => {
+    const title = req.body.title;
+
+    const insertSMT = `INSERT INTO playlist (P_Title) VALUES ('${title}') RETURNING *;`;
+
+    try {
+        const response = await pool.query(insertSMT);
+        console.log("Playlist created successfully:", response.rows[0]);
+        res.status(200).json(response.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Endpoint for fetching all playlists
+app.get("/api/playlists", async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM playlist');
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Endpoint for adding a song to a playlist
+app.post("/api/playlists/:playlistId/add-song", async (req, res) => {
+    const playlistId = req.params.playlistId;
+    const songId = req.body.songId;
+
+    const insertSMT = `
+        INSERT INTO "Contains" (P_ID, S_ID)
+        VALUES (${playlistId}, '${songId}')
+        ON CONFLICT DO NOTHING
+        RETURNING *;
+    `;
+
+    try {
+        const response = await pool.query(insertSMT);
+        if (response.rows.length > 0) {
+            console.log("Song added to playlist successfully:", response.rows[0]);
+            res.status(200).json(response.rows[0]);
+        } else {
+            console.log(`Song '${songId}' is already in playlist '${playlistId}'.`);
+            res.status(400).json({ error: `Song '${songId}' is already in playlist '${playlistId}'.` });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
