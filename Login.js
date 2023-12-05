4c8677ab5924638039c80774aa655afc568bb412
nshkulkarni <105520469+nshkulkarni@users.noreply.github.com>
2023-12-04 22:46:42 -0700

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './styles.css';
 
 function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const history = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useNavigate();
 
     const handleLogin = async () => {
         // Make a POST request to the backend for login
@@ -57,28 +58,28 @@ function Login() {
     };
 
     return (
-        <div>
-            <h2>Login or Create Account</h2>
-            <form>
-                <label>
-                    Username:
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                </label>
-                <br />
-                <button type="button" onClick={handleLogin}>
-                    Login
-                </button>
-                <button type="button" onClick={handleCreateAccount}>
-                    Create Account
-                </button>
-            </form>
+        <div className="login-container">
+          <h2>Jamix</h2>
+          <form>
+            <label className='label'>
+              Username:
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="rounded-input"/>
+            </label>
+            <br />
+            <label className='label'>
+              Password:
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="rounded-input"/>
+            </label>
+            <br />
+            <button type="button" className='btn' onClick={handleLogin}>
+              Login
+            </button>
+            <button type="button" className='btn' onClick={handleCreateAccount}>
+              Create Account
+            </button>
+          </form>
         </div>
-    );
-}
-
-export default Login;
+      );
+    }
+    
+    export default Login;
